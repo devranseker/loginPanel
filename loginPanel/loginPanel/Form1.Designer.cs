0668d4c723b5c80f87b89b07686a7a30929c2443
Devran ŞEKER <120737148+devranseker@users.noreply.github.com>
2023-10-07 22:33:00 +0300

    loginPanel
    C# ile Giriş Paneli

---
+++ loginPanel/loginPanel/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace loginPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            pictureBox2 = new PictureBox();
+            label4 = new Label();
+            label7 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 141);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 21);
+            label1.TabIndex = 0;
+            label1.Text = "IdNo : ";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(115, 138);
+            maskedTextBox1.Mask = "00000000000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(301, 29);
+            maskedTextBox1.TabIndex = 1;
+            maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(115, 190);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(301, 29);
+            textBox1.TabIndex = 2;
+            textBox1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 190);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Password :";
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.BackColor = SystemColors.InactiveCaption;
+            flowLayoutPanel2.Location = new Point(-1, 312);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(593, 34);
+            flowLayoutPanel2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(189, 238);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 41);
+            button1.TabIndex = 6;
+            button1.Text = "Giriş Yap";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(440, 194);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(154, 25);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Şİfremi Unuttum ";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(253, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 50);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(247, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 21);
+            label4.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe Script", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label7.Location = new Point(3, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(238, 27);
+            label7.TabIndex = 4;
+            label7.Text = "Online  Ders Giriş Paneli  ";
+            label7.Click += label7_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = SystemColors.InactiveCaption;
+            flowLayoutPanel1.Controls.Add(label7);
+            flowLayoutPanel1.Controls.Add(label4);
+            flowLayoutPanel1.Controls.Add(pictureBox2);
+            flowLayoutPanel1.Location = new Point(11, 2);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(583, 130);
+            flowLayoutPanel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(586, 415);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Login Giriş Paneli ";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private MaskedTextBox maskedTextBox1;
+        private TextBox textBox1;
+        private Label label2;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Button button1;
+        private CheckBox checkBox1;
+        private PictureBox pictureBox2;
+        private Label label4;
+        private Label label7;
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ loginPanel/loginPanel/Form1.cs
@@ -0,0 +1,35 @@
+namespace loginPanel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
